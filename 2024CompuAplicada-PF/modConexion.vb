a37036cba072d641ee48cabac515eff1942ef8ce
Anderson-Oloroso <henrikog4@gmail.com>
2024-09-26 10:59:23 -0600

    Updating 0.0

---
+++ 2024CompuAplicada-PF/modConexion.vb
@@ -0,0 +1,20 @@
+﻿Module modConexion
+    Public conexion As New OleDb.OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0;Data Source=CIUDADANOS.accdb")
+
+    Public Sub conect()
+        Try
+            conexion.Open()
+        Catch ex As Exception
+            MsgBox("Error en la conexión" & ex.Message, vbOKOnly + vbCritical, "Error en la conexión")
+        End Try
+    End Sub
+
+    Public Sub desconect()
+        Try
+            conexion.Close()
+        Catch ex As Exception
+            MsgBox("Error en la desconexión" & ex.Message, vbOKOnly + vbCritical, "Error en la desconexión")
+        End Try
+    End Sub
+
+End Module
