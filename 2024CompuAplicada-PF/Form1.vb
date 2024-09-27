beb8c9cee49f47816d178811e36c7a73d194b661
KELVIN-HR <raymundostiven180@gmail.com>
2024-09-27 10:02:34 -0600

    Updating 0.5

--- 2024CompuAplicada-PF/Form1.vb
+++ 2024CompuAplicada-PF/Form1.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿Public Class frmmenuprincipal
+    Private Sub EncargadoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EncargadoToolStripMenuItem.Click
+        mdiEncargado.Show()
+    End Sub
 
+    Private Sub OperadorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OperadorToolStripMenuItem.Click
+        mdiOperador.Show()
+    End Sub
+
+    Private Sub AdministradorToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles AdministradorToolStripMenuItem1.Click
+        mdiAdministrador.Show()
+    End Sub
 End Class
