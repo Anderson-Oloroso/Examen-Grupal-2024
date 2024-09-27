9b9163a94a1e5146b7ed4797771c76120cf764d6
Anderson-Oloroso <henrikog4@gmail.com>
2024-09-27 11:45:38 -0600

    Updating 1.0

---
+++ 2024CompuAplicada-PF/frmCiudadanos.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmCiudadanos
+    Private Sub txtNombre_TextChanged(sender As Object, e As EventArgs) Handles txtNombre.TextChanged
+
+    End Sub
+
+    Private Sub frmCiudadanos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BackColor = Color.AliceBlue
+    End Sub
+End Class
